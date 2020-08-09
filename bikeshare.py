695d7af1ede26e43f30813433c73aeed74544115
King Carl Tornam Duho <kctduho@gmail.com>
2020-08-09 06:56:22 +0000

    Question 1 codes

--- bikeshare.py
+++ bikeshare.py
@@ -8,3 +8,24 @@ head(ny)
 head(wash)
 
 head(chi)
+
+
+#Question 1
+#Using the data on Chicago, create a binary plot using ggplot with Start.Time as x axis and Trip.Duration as y axis. The y axis should range from 0 to 24,000. What are your observations?
+# To load ggplot2
+library(ggplot2)
+
+#The ploting code for answering the question
+p <- ggplot(aes(x =Start.Time, y= Trip.Duration), data=chi)
+
+p +
+  geom_point(aes(colour = Gender)) +
+    ggtitle('Nexus Between Start Time and Trip Duration')+
+  scale_colour_manual(values = c("orange", "forestgreen", "blue")) +
+  coord_cartesian(ylim=c(0,24000))
+
+#To present relevant summary statistics
+summary(chi$Trip.Duration)
+by(chi$Trip.Duration, chi$Gender, summary)
+
+#Summary of your question 1 results goes here. The result (on Chicago) projects the relationship between the trip duration and the start time, separated based on gender. The data diagram shows the results including the NA which are coloured as yellow. Majority of the NA datasets are results above 5000 trip duration. On avregare, femeles have the higher trip duration in Chicago.
